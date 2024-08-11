6d09f3147a54ac7bcba90f30d000c37c4b58632f
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-11 20:41:51 +0530

    modify navbar

--- astrophysics-club/components/globals/Navbar.jsx
+++ astrophysics-club/components/globals/Navbar.jsx
@@ -3,23 +3,27 @@ import "./Navbar.css";
 export default function Navbar() {
     return (
         <nav className="navigation">
-            <picture className="nav-logo">
-                <img
-                    src="/assets/club-logo.png"
-                    alt="Astrophysics Club Logo"
-                />
-            </picture>
-            <ul className="nav-links">
-                <li className="nav-link">
-                    <a href="/">Home</a>
-                </li>
-                <li className="nav-link">
-                    <a href="/">Projects</a>
-                </li>
-                <li className="nav-link">
-                    <a href="/">Crew</a>
-                </li>
-            </ul>
+            <div>
+                <picture className="nav-logo">
+                    <img
+                        src="/assets/club-logo.png"
+                        alt="Astrophysics Club Logo"
+                    />
+                </picture>
+            </div>
+            <div className="nav-content">
+                <ul className="nav-links">
+                    <li className="nav-link">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="nav-link">
+                        <a href="/">Projects</a>
+                    </li>
+                    <li className="nav-link">
+                        <a href="/">Crew</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
