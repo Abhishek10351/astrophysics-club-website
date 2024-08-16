1b4534b6582bbc42297c9d8dfc97b00ca4a45790
Abhishek Kashyap <abhik5567@gmail.com>
2024-08-16 16:32:20 +0530

    move navbar component

--- astrophysics-club/sites/home/Home.jsx
+++ astrophysics-club/sites/home/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../../components/globals/Navbar";
+import Navbar from "../../components/globals/Navbar/Navbar";
 import "./Home.css";
 export default function Home() {
     return (
